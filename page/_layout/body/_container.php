1d3f2ff8b1acbe384c675ba74e0f8f2627a35de8
nev3rmi <nev3rmi@gmail.com>
2016-12-09 13:22:23 +0700

    Update Page

--- page/_layout/body/_container.php
+++ page/_layout/body/_container.php
@@ -1,6 +1,12 @@
+<div class="upper-containner">
+<?php require_once $thisPageUpperContent ?>
+</div>
 <div class="container">
 <hr>
 <?php require_once $thisPageContent ?>
 <hr>
 <?php require_once $_phpPath."page/_layout/body/_footer.php"; ?>
+</div>
+<div class="downer-containner">
+<?php require_once $thisPageDownerContent ?>
 </div>
--- page/_layout/body/_footer.php
+++ page/_layout/body/_footer.php
@@ -1,7 +1,7 @@
 <footer>
   <div class="row">
     <div class="col-lg-12">
-      <p>Copyright &copy; Your Website 2014</p>
+      <p>Copyright &copy; <?php echo $_sitePublisher." ".$_siteCopyrightYear?></p>
     </div>
   </div>
   <!-- /.row --> 
