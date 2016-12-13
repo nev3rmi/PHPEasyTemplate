8978492b6c3ca3eea8b20c5fd1e0911bdb8afa5b
NeV3RmI <nev3rmi@gmail.com>
2016-12-13 09:38:25 +0700

    HTACCESS Force to use HTTPS

--- index.php
+++ index.php
@@ -1,4 +1,3 @@
 <?php
 require_once realpath('.')."/page/index/indexContent.php";
-consoleData((isset($_SERVER['HTTPS']) && $_SERVER['HTTPS'] != 'off') ?$protocol = "https": $protocol = "http");
 ?>
