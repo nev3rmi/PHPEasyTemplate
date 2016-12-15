77c53f9530fd9f23fd45bb0b31cfbf49114b07ad
NeV3RmI <nev3rmi@gmail.com>
2016-12-15 11:50:41 +0700

    Pass login parameter

--- page/_layout/head/_generalmeta.php
+++ page/_layout/head/_generalmeta.php
@@ -1,4 +1,5 @@
 <meta name="viewport" content="width=device-width, initial-scale=1.0">
+<link rel="icon" href="<?php echo $_url;?>favicon.ico">
 <?php
 echo $_thisPageMeta;
 ?>
--- page/user/login/controller/validatelogin.php
+++ page/user/login/controller/validatelogin.php
@@ -1,6 +1,10 @@
 <?php include_once realpath($_SERVER["DOCUMENT_ROOT"])."/setting/control.php"; ?>
 <?php
-// Not work yet
-parse_str($_POST["formValue"], $data);
-consoleData($data);
+$email = htmlentities($_POST['userEmail'], ENT_QUOTES, "UTF-8");
+$password =  htmlentities($_POST['userPassword'], ENT_QUOTES, "UTF-8");
+
+if (!RegexCheck($regexMail,$email)){
+	exit();
+}
+
 ?>
