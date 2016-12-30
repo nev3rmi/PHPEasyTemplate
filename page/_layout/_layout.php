238a1096cc5b9ad64f0b47c360639ed87f5f1d82
NeV3RmI <nev3rmi@gmail.com>
2016-12-31 01:41:36 +0700

    Update $_Security

--- page/_layout/_layout.php
+++ page/_layout/_layout.php
@@ -1,6 +1,23 @@
-<?php require_once $_SERVER['DOCUMENT_ROOT']."/setting/control.php"; ?>
-<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
-<html xmlns="http://www.w3.org/1999/xhtml">
-<?php require_once $_phpPath."page/_layout/_head.php"; ?>
-<?php require_once $_phpPath."page/_layout/_body.php"; ?>
-</html>
+<?php require_once $_SERVER['DOCUMENT_ROOT']."/setting/config.php"; ?>
+<?php
+class Page(){
+	function SystemPath($thisPagePath){
+		return $_phpPath.$thisPagePath;
+	}
+	function Name($thisPageName){
+		return $thisPageName;
+	}
+	function Meta($thisPageMeta){
+		return $thisPageMeta;
+	}
+	function Heading($thisPageHeading){
+		return $thisPageHeading;
+	}
+	function SubHeading($thisPageSubHeading){
+		return $thisPageSubHeading;
+	}
+	function Controller(){
+		
+	}
+}
+?>
